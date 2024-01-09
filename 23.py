7b08fe06173984e8629b4d7312f343fe2bcf7c41
ap <59622720+arspaper@users.noreply.github.com>
2024-01-09 06:23:33 +0300

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,21 +1,34 @@
-def f(x, y):  # Trajectory: include 10
+def F(x, y):  # Trajectory: exclude 24, include 10
+    if x == y:
+        return 1
+    if x > y or x == 14:
+        return 0
+    if x < y:
+        return F(x + 1, y) + F(x + 2, y) + F(x * 3, y)
+
+
+print(F(2, 10) * F(10, 15))
+
+
+def F(x, y):  # Trajectory: include 10
     if x < y:
         return 0
     if x == y:
         return 1
     else:
-        return f(x - 2, y) + f(x // 2, y)
+        return F(x - 2, y) + F(x // 2, y)
 
 
-print(f(28, 10) * f(10, 1))
+print(F(28, 10) * F(10, 1))
 
 
 def F(x, y):  # Trajectory: include 9, 11
-    if x == y: return 1
-
-    if x > y: return 0
-
-    if x < y: return F(x + 1, y) + F(x * 3, y) + F(x + 2, y)
+    if x == y:
+        return 1
+    if x > y:
+        return 0
+    if x < y: 
+        return F(x + 1, y) + F(x * 3, y) + F(x + 2, y)
 
 
 print(F(2, 9) * F(9, 11) * F(11, 12))
@@ -38,5 +51,6 @@ def F(x, y):
     if x > y: return 0
 
     if x < y: return F(x+3, y) + F(x*2, y)
+
 
 print(F(1, 25))
